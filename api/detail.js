1d74ae2c68096b0d1296d34594b32f5d64ebf2ad
marslan-786 <marslansalfias@gmail.com>
2025-11-28 09:23:15 +0500

    Create detail.js

---
+++ api/detail.js
@@ -0,0 +1,18 @@
+{
+  "name": "ufone-claim-api",
+  "version": "1.0.0",
+  "description": "Serverless API for encrypted OTP generation",
+  "type": "module",
+  "main": "index.js",
+  "scripts": {
+    "dev": "vercel dev",
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "axios": "^1.6.7",
+    "node-fetch": "^3.3.2",
+    "next": "^14.0.0",
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0" 
+  }
+}
